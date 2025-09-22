d2b8941a140cf33bf5367d352f31c0620b53e2b4
kaa004-zealand <kaa004@edu.zealand.dk>
2025-09-22 11:12:32 +0200

    Task 2

---
+++ Bridge/Car.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bridge
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class Car
+    {
+        /// <summary>
+        /// The licenseplate of the ticketed vehicle.
+        /// </summary>
+        public string Licenseplate;
+        /// <summary>
+        /// The Date of the ticket.
+        /// </summary>
+        public DateTime Date;
+
+        /// <summary>
+        /// Returns the price of a ticket.
+        /// </summary>
+        /// <returns>230</returns>
+        public double Price() => 230;
+        /// <summary>
+        /// Returns the type of vehicle.
+        /// </summary>
+        /// <returns>"Car"</returns>
+        public string VehicleType() => "Car";
+    }
+}
---
+++ TestProject/TestCar.cs
@@ -0,0 +1,39 @@
+﻿using Bridge;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject
+{
+    [TestClass]
+    public class TestCar
+    {
+        [TestMethod]
+        public void TestPrice()
+        {
+            //Arrange
+            Car car = new Car();
+
+            //Act
+            double price = car.Price();
+
+            //Assert
+            Assert.AreEqual(230, price, 0.01);
+        }
+
+        [TestMethod]
+        public void TestVehicleType()
+        {
+            //Arrange
+            Car car = new Car();
+
+            //Act
+            string vehicleType = car.VehicleType();
+
+            //Assert
+            Assert.AreEqual("Car", vehicleType);
+        }
+    }
+}
